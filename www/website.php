df5ef5c625bb7a5d0e55ce3b6f7da9442ae530bf
K M Tso <kmtso@live.com>
2022-06-02 23:29:32 +0800

    constructor1

--- www/website.php
+++ www/website.php
@@ -7,7 +7,27 @@
   <body>
 
 <?php include "header.html" ?>
-    
+
+<?php
+class Book {
+  var $title;
+  var $author;
+  var $pages;
+  function __construct(){
+    echo "New Book Created<br>";
+  }
+}
+  $book1 = new Book;
+  $book1 -> title=" Harry Potter";
+  $book1 -> author= "JK ROWLING";
+  $book1 -> pages =400;
+  echo "$book1->title<br>";
+  $book2 = new Book;
+  $book2 -> title="Lord of the Rings";
+  $book2 -> author= "Tolkien";
+  $book2 -> pages =700;
+  echo "$book2->title";
+?>
 <?php include "footer.html" ?>
 
                  </body>
