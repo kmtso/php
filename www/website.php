cc1cf266eedf662448140fc9c759b690584a68a5
K M Tso <kmtso@live.com>
2022-06-03 00:16:21 +0800

    Getters and Setters

--- www/website.php
+++ www/website.php
@@ -13,10 +13,12 @@ class Book {
   var $title;
   var $author;
   var $pages;
-  function __construct($title,$author,$pages){
+  private $rating;
+  function __construct($title,$author,$pages,$rating){
     $this -> title=$title;
     $this -> author=$author;
     $this -> pages =$pages;
+    $this -> rating = $rating;
   }
 
   function getPagesRange(){
@@ -26,13 +28,28 @@ class Book {
       return "larger than 500 pages";
     }
   }
+  function getRating(){
+    echo $this->rating;
+  }
+  function SetRating($rating){
+    if ($rating>=0&&$rating<=5)
+      $this->rating=$rating;
+    else
+      $this->rating=-1;
+  }
 }
-  $book1 = new Book("Harry Potter","JK ROWLING",700);
+  $book1 = new Book("Harry Potter","JK ROWLING",700,5);
 
   echo "$book1->title<br>";
   echo $book1->getPagesRange();
-
-?>
+  echo "<br>";
+  echo $book1->getRating();
+  echo "<br>";
+  $book1->setRating(1);
+  echo $book1->getRating();
+  echo "<br>";
+  $book1->setRating(100);
+  echo $book1->getRating();?>
 <?php include "footer.html" ?>
 
                  </body>
