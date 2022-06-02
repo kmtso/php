0bb6687c9383563fd84a3e9e370b99d2cc3e7e9b
K M Tso <kmtso@live.com>
2022-06-02 23:02:55 +0800

    include

--- www/website.php
+++ www/website.php
@@ -5,44 +5,10 @@
     <title></title>
   </head>
   <body>
-    <form action ="website.php" method="post">
-      What is your grade?<br>
-      <input type='text' name= 'grade'><br>
 
-  <input type="submit">
-
-
-    </form>
-               <?php
-
-                $grade = $_POST['grade'];
-                $grade = strtoupper($grade);
-                switch ($grade) {
-                  case 'A':
-                     echo "excellent";
-                    break;
-                  case 'B':
-                    echo "Good";
-                    break;
-
-                  case 'C':
-                      echo "Average";
-                      break;
-
-                  case 'D':
-                    echo "Pass";
-                    break;
-
-                    case 'F':
-                      echo "Fail";
-                      // code...
-                      break;
-                    default:
-                    // code...
-                    echo "invalid grade";
-                    break;
-                }
-   ?>
+<?php include "header.html" ?>
+    
+<?php include "footer.html" ?>
 
                  </body>
 </html>
